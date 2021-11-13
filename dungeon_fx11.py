c7ae65d0ff9ee332b32a7cfa9939384ace23e493
Pedro-Juan “vehrka” Ferrer Matoses <vehrka@gmail.com>
2021-11-13 17:07:03 +0100

    Directed graph and OO step

--- dungeon_fx11.py
+++ dungeon_fx11.py
@@ -7,7 +7,7 @@ from utils.dice_roller import DiceRoller
 
 class Dungeon:
     def __init__(self, dtype="p24"):
-        self.__g = igraph.Graph()
+        self.__g = igraph.Graph(directed=True)
         self.__dice = DiceRoller()
         self.__dungeon_type = dtype
         self.generate()
@@ -15,16 +15,18 @@ class Dungeon:
 
     def generate(self):
         self.__g.add_vertices(2)
-        self.__g.vs["name"] = ["START", "GOAL"]
-        self.__g.vs["type"] = ["START", "GOAL"]
-        self.__g.vs["color"] = ["green", "green"]
+        self.__format_nv(self.__g.vs[0], 'START', 'green', 'START-0')
+        self.__format_nv(self.__g.vs[1], 'GOAL', 'green', 'GOAL-1')
+
         if self.__dungeon_type == "p24":
             self.__dungeonstart24()
-            self.__process()
-            self.get_dungeon()
-            self.get_graph()
         else:
             print("Post 30 dungeon type still under development")
+            return
+
+        self.__process()
+        self.get_dungeon()
+        self.get_graph()
         return
 
     def get_dungeon(self):
@@ -57,22 +59,21 @@ class Dungeon:
             # for each red vertex, process its type
             for rv in red_vertex:
                 rv_type = rv["type"][:2]
-                nei = self.__g.neighbors(rv)
-                voi = min(nei)
-                vdi = max(nei)
                 if rv_type == "OM":
-                    self.__generate_OM(rv, voi, vdi)
+                    self.__generate_OM(rv)
+                elif rv_type == "OO":
+                    self.__generate_OO(rv)
             # max num of iter
-            if niter > 0:
+            if niter > 3:
                 break
 
     def __dungeonstart24(self):
         """Generates the initial layout of the dungeon"""
-        sv = self.__g.vs.find("START").index
-        gv = self.__g.vs.find("GOAL").index
+        sv = self.__g.vs[0]
+        gv = self.__g.vs[1]
         basic_layout_roll = self.__dice.d20
 
-        #  basic_layout_roll = 1
+        basic_layout_roll = 1
 
         if basic_layout_roll <= 5:
             nv = self.__insert_room_btw(sv, gv, "OM(?)")
@@ -97,15 +98,31 @@ class Dungeon:
             self.__insert_room_btw(sv, nv_ui, "OL(?)")
         return
 
-    def __generate_OM(self, rv, voi, vdi):
+    def __generate_OO(self, rv):
+        """One key to one lock
+
+        Attrs:
+            rv int:  Reference vertex
+
+        """
+        voi = self.__g.neighbors(rv, 'in')[0]
+        vdi = self.__g.neighbors(rv, 'out')[0]
+        nv_n = self.__insert_room_btw(voi, vdi, "n", color="green")
+        self.__insert_room_end(nv_n, "UI(i5)")
+        nv_ol = self.__insert_room_btw(nv_n, vdi, "OL(i5)")
+        self.__insert_room_btw(nv_ol, vdi, "eb", color="green")
+        self.__g.delete_vertices(rv)
+        return
+
+    def __generate_OM(self, rv):
         """One key to many lock
 
         Attrs:
             rv int:  Reference vertex
-            voi int: Vertex Origin Index
-            vdi int: Vertex Destination Index
 
         """
+        voi = self.__g.neighbors(rv, 'in')[0]
+        vdi = self.__g.neighbors(rv, 'out')[0]
         nv_n = self.__insert_room_btw(voi, vdi, "n", color="green")
         self.__insert_room_end(nv_n, "UI(?)")
         nv_ml = self.__insert_room_end(nv_n, "ML(?)")
@@ -152,7 +169,7 @@ class Dungeon:
         """
         nv = self.__g.add_vertex()
         self.__format_nv(nv, type, color, name)
-        self.__g.add_edges([(nv.index, vdi)])
+        self.__g.add_edges([(vdi, nv.index)])
         return nv.index
 
     def __format_nv(self, nv, type, color, name):
@@ -160,9 +177,9 @@ class Dungeon:
         nv["type"] = type
         nv["color"] = color
         if not name:
-            #  name = self.__random_string()
-            name = type
+            name = f'{type}-{nv.index}'
         nv["name"] = name
+        nv["uid"] = self.__random_string(8)
         return
 
     def __random_string(self, lenght=5):
